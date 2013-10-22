6c5e722db4d563e3154a20356e739f79284380a7
Quinn Slack <qslack@qslack.com>
2013-10-22 11:40:29 -0700

    Add example

--- README.md
+++ README.md
@@ -4,6 +4,23 @@ createsend-go
 createsend-go is a [Go](http://golang.org) library for accessing the [Campaign
 Monitor API](http://www.campaignmonitor.com/api/).
 
+Running the tests
+-----------------
+
+To run the tests:
+
+```
+go test ./createsend
+```
+
+To run the included example (in `createsend/example_test.go`), set your Campaign
+Monitor API key in the `API_KEY` environment variable (available in Account
+Settings).
+
+```
+API_KEY=your-api-key go test ./createsend
+```
+
 Acknowledgements
 ----------------
 
---
+++ createsend/example_test.go
@@ -0,0 +1,37 @@
+package createsend_test
+
+import (
+	"fmt"
+	"github.com/sourcegraph/createsend-go/createsend"
+	"net/http"
+	"os"
+)
+
+func Example() {
+	apiKey := os.Getenv("API_KEY")
+	if apiKey == "" {
+		fmt.Fprintf(os.Stderr, "You must set your Campaign Monitor API key in the API_KEY environment variable to run example_test.go. (Skipping.)\n")
+		os.Exit(0)
+	}
+
+	authClient := &http.Client{
+		Transport: &createsend.APIKeyAuthTransport{APIKey: apiKey},
+	}
+
+	c := createsend.NewAPIClient(authClient)
+	clients, err := c.ListClients()
+	if err != nil {
+		fmt.Printf("Error listing clients: %s\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Found %d clients.\n", len(clients))
+	for _, client := range clients {
+		fmt.Printf(" - %s (ID: [%d-char ID])\n", client.Name, len(client.ClientID))
+	}
+
+	// This output will be different for each account.
+
+	// output:
+	// Found 1 clients.
+	//  - Sourcegraph (ID: [32-char ID])
+}
